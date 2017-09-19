8d8234e00b7f503fbedbbcd6ff5cbe7b090323ab
blakethomas <blakekthomas49@gmail.com>
2017-09-19 15:52:12 -0700

    Adds cancel button for checkout screen.

--- main.js
+++ main.js
@@ -1,103 +1,103 @@
 var products = [
-{
-  name:'Ethiopian Coffee',
-  price:12.95,
-  imgSrc:'https://images.pexels.com/photos/34085/pexels-photo.jpg?h=350&auto=compress&cs=tinysrgb',
-  details: {
-    origin:'Addis Abada, Ethiopia',
-    description: 'Has a strong kick and keeps you up all morining.',
-    aroma:'Chesnuts, Dark Chocolate, Tobacco',
-    ammount: '1 lbs'
+  {
+    name:'Ethiopian Coffee',
+    price:12.95,
+    imgSrc:'https://images.pexels.com/photos/34085/pexels-photo.jpg?h=350&auto=compress&cs=tinysrgb',
+    details: {
+      origin:'Addis Abada, Ethiopia',
+      description: 'Has a strong kick and keeps you up all morining.',
+      aroma:'Chesnuts, Dark Chocolate, Tobacco',
+      ammount: '1 lbs'
+    },
   },
-},
-{
-  name:'Colombian Coffee',
-  price:15.95,
-  imgSrc:'https://images.pexels.com/photos/302899/pexels-photo-302899.jpeg?h=350&auto=compress&cs=tinysrgb',
-  details: {
-    origin:'Bogota, Colombia',
-    description: 'A mild, well balanced bean for all types of cofffee lovers.',
-    aroma:'Caramel, Nuts, Butter',
-    ammount: '1 lbs'
+  {
+    name:'Colombian Coffee',
+    price:15.95,
+    imgSrc:'https://images.pexels.com/photos/302899/pexels-photo-302899.jpeg?h=350&auto=compress&cs=tinysrgb',
+    details: {
+      origin:'Bogota, Colombia',
+      description: 'A mild, well balanced bean for all types of cofffee lovers.',
+      aroma:'Caramel, Nuts, Butter',
+      ammount: '1 lbs'
+    }
+  },
+  {
+    name:'Espresso Machine',
+    price:1999.95,
+    imgSrc:'https://images.pexels.com/photos/324028/pexels-photo-324028.jpeg?h=350&auto=compress&cs=tinysrg',
+    details: {
+      origin:'Florence, Italy',
+      description: 'A machine made to satisfy all your espresso needs.',
+      aroma:'N/A',
+      ammount: '1 Unit'
+    }
+  },
+  {
+    name:'Vietnamese Coffee',
+    price:12.95,
+    imgSrc:'https://images.pexels.com/photos/6067/coffee-flower-reading-magazine.jpg?h=350&auto=compress&cs=tinysrgb',
+    details: {
+      origin:'Ho Chi Minh, Vietnam',
+      description: 'A bold, caffeine filled bean that is for experienced coffee drinkers',
+      aroma:'Toffee, Hazelnut',
+      ammount: '1 lbs'
+    }
+  },
+  {
+    name:'Hawaiian Coffee',
+    price:14.95,
+    imgSrc:'https://images.pexels.com/photos/2059/restaurant-red-beans-coffee.jpg?h=350&auto=compress&cs=tinysrgb',
+    details: {
+      origin:'Kawaii, Hawaii',
+      description: 'A kona blend for those who love that traditional Hawaiian taste.',
+      aroma:'Caramel, Cashew',
+      ammount: '1 lbs'
+    }
+  },
+  {
+    name:'Jamaican Coffee',
+    price:16.95,
+    imgSrc:'https://images.pexels.com/photos/544117/pexels-photo-544117.jpeg?h=350&auto=compress&cs=tinysrgb',
+    details: {
+      origin:'Kingston, Jamaica',
+      description: 'A delicious bean, complimented by perfect herbal flavor notes.',
+      aroma:'Herbal',
+      ammount: '1 lbs'
+    }
+  },
+  {
+    name:'French Press',
+    price:99.95,
+    imgSrc:'https://cdn.gearpatrol.com/wp-content/uploads/2016/03/better-coffee-at-home-gear-patrol-french-press.jpg',
+    details: {
+      origin:'Nice, France',
+      description: 'A machine made for the ultimate cup of joe.',
+      aroma:'N/A',
+      ammount: '1 Unit'
+    }
+  },
+  {
+    name:'Cold Brew Maker',
+    price:49.95,
+    imgSrc:'https://cdn.gearpatrol.com/wp-content/uploads/2016/03/better-coffee-at-home-gear-patrol-cold-brew-maker.jpg',
+    details:{
+      origin:'Los Angeles, United States',
+      description: 'A contraption for making coffee with cold water.',
+      aroma:'N/A',
+      ammount: '1 Unit'
+    }
+  },
+  {
+    name:'Automated Coffee Machine',
+    price:399.95,
+    imgSrc:'https://cdn.gearpatrol.com/wp-content/uploads/2016/03/better-coffee-at-home-gear-patrol-coffee-maker.jpg',
+    details: {
+      origin:'Philadelphia, United States',
+      description: 'A machine made that does all the work for you.',
+      aroma:'N/A',
+      ammount: '1 Unit'
+    }
   }
-},
-{
-  name:'Espresso Machine',
-  price:1999.95,
-  imgSrc:'https://images.pexels.com/photos/324028/pexels-photo-324028.jpeg?h=350&auto=compress&cs=tinysrg',
-  details: {
-    origin:'Florence, Italy',
-    description: 'A machine made to satisfy all your espresso needs.',
-    aroma:'N/A',
-    ammount: '1 Unit'
-  }
-},
-{
-  name:'Vietnamese Coffee',
-  price:12.95,
-  imgSrc:'https://images.pexels.com/photos/6067/coffee-flower-reading-magazine.jpg?h=350&auto=compress&cs=tinysrgb',
-  details: {
-    origin:'Ho Chi Minh, Vietnam',
-    description: 'A bold, caffeine filled bean that is for experienced coffee drinkers',
-    aroma:'Toffee, Hazelnut',
-    ammount: '1 lbs'
-  }
-},
-{
-  name:'Hawaiian Coffee',
-  price:14.95,
-  imgSrc:'https://images.pexels.com/photos/2059/restaurant-red-beans-coffee.jpg?h=350&auto=compress&cs=tinysrgb',
-  details: {
-    origin:'Kawaii, Hawaii',
-    description: 'A kona blend for those who love that traditional Hawaiian taste.',
-    aroma:'Caramel, Cashew',
-    ammount: '1 lbs'
-  }
-},
-{
-  name:'Jamaican Coffee',
-  price:16.95,
-  imgSrc:'https://images.pexels.com/photos/544117/pexels-photo-544117.jpeg?h=350&auto=compress&cs=tinysrgb',
-  details: {
-    origin:'Kingston, Jamaica',
-    description: 'A delicious bean, complimented by perfect herbal flavor notes.',
-    aroma:'Herbal',
-    ammount: '1 lbs'
-  }
-},
-{
-  name:'French Press',
-  price:99.95,
-  imgSrc:'https://cdn.gearpatrol.com/wp-content/uploads/2016/03/better-coffee-at-home-gear-patrol-french-press.jpg',
-  details: {
-    origin:'Nice, France',
-    description: 'A machine made for the ultimate cup of joe.',
-    aroma:'N/A',
-    ammount: '1 Unit'
-  }
-},
-{
-  name:'Cold Brew Maker',
-  price:49.95,
-  imgSrc:'https://cdn.gearpatrol.com/wp-content/uploads/2016/03/better-coffee-at-home-gear-patrol-cold-brew-maker.jpg',
-  details:{
-    origin:'Los Angeles, United States',
-    description: 'A contraption for making coffee with cold water.',
-    aroma:'N/A',
-    ammount: '1 Unit'
-  }
-},
-{
-  name:'Automated Coffee Machine',
-  price:399.95,
-  imgSrc:'https://cdn.gearpatrol.com/wp-content/uploads/2016/03/better-coffee-at-home-gear-patrol-coffee-maker.jpg',
-  details: {
-    origin:'Philadelphia, United States',
-    description: 'A machine made that does all the work for you.',
-    aroma:'N/A',
-    ammount: '1 Unit'
-  }
-}
 ]
 
 var counter = 0
@@ -133,7 +133,12 @@ var $addressInput = document.createElement('input')
 var $phoneInput =  document.createElement('input')
 var $cardInput = document.createElement('input')
 var $expirationInput = document.createElement('input')
+var $exitCheckout = document.createElement('button')
+var $btnRow = document.createElement('div')
 
+$btnRow.classList.add('row')
+$exitCheckout.classList.add('btn', 'btn-default', 'w-30', 'mx-auto')
+$exitCheckout.textContent = 'Close'
 $exit.classList.add('card', 'row', 'mb-5', 'border-0')
 $cartTable.classList.add('table', 'p-2', 'border-0')
 $priceRow.setAttribute('scope', 'row')
@@ -181,7 +186,9 @@ $checkoutButton.addEventListener('click', function(){
   $purchaseForm.appendChild($cardInput)
   $purchaseForm.appendChild($expirationInput)
   $checkoutScreen.appendChild($purchaseForm)
+
   $checkoutScreen.appendChild($completePurchase)
+  $checkoutScreen.appendChild($exitCheckout)
 })
 
 
@@ -206,7 +213,7 @@ function renderCart(product){
   $cartRow.setAttribute('scope', 'row')
 
 
-/*Listens for removal of items*/
+  /*Listens for removal of items*/
   $removeButton.addEventListener('click', function(){
     $cartRow.textContent = ''
     counter = counter - 1
@@ -257,7 +264,7 @@ function renderDetails(product){
   $addButton.classList.add('btn', 'btn-default', 'add-button', 'mt-3', 'mr-4')
   $addButton.textContent = 'Add To Cart'
 
-/*Listens for the add to cart button*/
+  /*Listens for the add to cart button*/
   $addButton.addEventListener('click', function(){
     cart.push(product)
     counter ++
